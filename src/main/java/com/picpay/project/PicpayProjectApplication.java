230b81323f7abc8ef015580816e6227587cc272e
SilasSilvaa <silas-oli@hotmail.com>
2024-11-28 00:03:37 -0300

    feat: first commit

---
+++ src/main/java/com/picpay/project/PicpayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayProjectApplication.class, args);
+	}
+
+}
